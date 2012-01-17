7b8fbb6b6b2d96958cddf83d1cc71e7bd16e810e
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-01-17 20:31:25 +0100

    minor change

--- ARCADE/airborne/components/interfaces/mavlink/mavlink_udp_test.py
+++ ARCADE/airborne/components/interfaces/mavlink/mavlink_udp_test.py
@@ -2,6 +2,7 @@
 import random
 import time
 import sys
+import time
 
 from monitor_data_pb2 import CoreMonData
 from pymavlink.mavlinkv10 import *
@@ -10,15 +11,14 @@ from pymavlink.mavutil import mavudp
 mav_udp = mavudp('localhost:14550', False)
 link = MAVLink(mav_udp)
 
-socket = generate_map('mavlink')['core_mon']
+i = 0.0
 mon = CoreMonData()
-
 while True:
+   i += 0.000001
    time_ms = int(time.time() / 10)
    flags = MAV_MODE_FLAG_AUTO_ENABLED | MAV_MODE_FLAG_STABILIZE_ENABLED | MAV_MODE_FLAG_SAFETY_ARMED
    link.heartbeat_send(MAV_TYPE_QUADROTOR, MAV_AUTOPILOT_GENERIC, flags, 1, MAV_STATE_ACTIVE)
-   link.gps_global_origin_send(int(50.0 * 10e7), int(10.0 * 10e7), 525.0)
-   link.local_position_ned_send(time_ms, mon.x, mon.y, mon.z, mon.x_speed, mon.y_speed, mon.z_speed)
-   link.attitude_send(time_ms, mon.roll, mon.pitch, mon.yaw, mon.roll_speed, mon.pitch_speed, mon.yaw_speed)
-
+   link.global_position_int_send(time_ms, int((50.0 + i) * 1e7), int(10.0 * 1e7), 500, 0, 0, 0, 0, 0)
+   link.attitude_send(time_ms, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+   time.sleep(0.1)
 
