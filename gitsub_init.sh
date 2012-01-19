b85551a51d664c6c8f0776e1ac496aa42fe97856
Tobias Simon <tobi@eeepc-1.(none)>
2012-01-19 23:03:01 +0100

    to be tested

--- gitsub_init.sh
+++ gitsub_init.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-cp .gitmodules_ro .gitmodules
-cp .gitmodules_rw .gitmodules
+rw=`cat .git/config | grep git@`
+if ["$rw" -eq ""]; then
+   echo t
+   cp .gitmodules_ro .gitmodules
+else
+   echo u
+   cp .gitmodules_rw .gitmodules
+fi
 
-git submodule init
-git submodule update
-git submodule foreach git checkout master
+
+#git submodule init
+#git submodule update
+#git submodule foreach git checkout master
