a425f59a463ace7526731af3dc56a5ead1ffcdb0
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-01-20 07:41:10 +0100

    edit

--- gitsub_init.sh
+++ gitsub_init.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-rw=`cat .git/config | grep git@`
-if ["$rw" -eq ""]; then
-   echo t
+cat .git/config | grep git@ > /dev/null
+if [ "$?" != "0" ]; then
    cp .gitmodules_ro .gitmodules
 else
-   echo u
+   echo rw
    cp .gitmodules_rw .gitmodules
 fi
 
-
-#git submodule init
-#git submodule update
-#git submodule foreach git checkout master
+git submodule init
+git submodule update
+git submodule foreach git checkout master
