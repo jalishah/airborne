3f2d206f0fa8de4c15683a0f1bee85f7f02435ef
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-01-20 07:48:59 +0100

    added verbose output

--- gitsub_init.sh
+++ gitsub_init.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 cat .git/config | grep git@ > /dev/null
+echo -n "detected "
 if [ "$?" != "0" ]; then
+   echo -n non-
    cp .gitmodules_ro .gitmodules
 else
-   echo rw
    cp .gitmodules_rw .gitmodules
 fi
+echo authenticated configuration, applying to submodules
 
 git submodule init
 git submodule update
