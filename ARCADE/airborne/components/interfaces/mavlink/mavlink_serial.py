9528bce6dc02064877ea8659d0d20b496a24d085
Tobias Simon <tobias.simon@tu-ilmenau.de>
2012-02-02 16:48:05 +0100

    added some mavlink functionality

--- ARCADE/airborne/components/interfaces/mavlink/mavlink_serial.py
+++ ARCADE/airborne/components/interfaces/mavlink/mavlink_serial.py
@@ -18,45 +18,45 @@ class ParamHandler(Thread):
       self.dispatcher = dispatcher
       self.opcd_interface = OPCD_Interface('mavlink')
       self.param_map = {}
+      self.param_rev_map = {}
       list = self.opcd_interface.get('')
       c = 0
-      type_map = {float: 9, long: 6}
+      type_map = {float: MAV_VAR_FLOAT, long: MAV_VAR_INT32}
+      cast_map = {float: float, long: int}
       for name, val in list:
          try:
             type = type_map[val.__class__]
-            self.param_map[c] = name, type
+            self.param_map[c] = name, type, cast_map[val.__class__]
+            self.param_rev_map[c] = type, name, cast_map[val.__class__]
             c += 1
-         except:
-            # string
-            pass
+         except Exception, e:
+            print str(e)
    
    def run(self):
       for e in self.dispatcher.generator('PARAM_'):
          if e.get_type() == 'PARAM_REQUEST_LIST':
             print e
             list = self.opcd_interface.get('')
-            for index, (name, type) in self.param_map.items():
+            for index, (name, type, cast) in self.param_map.items():
                try:
                   val = self.opcd_interface.get(name)
-                  name_short = re.sub('(?P<foo>\w)\w*\.', '\g<foo>.', name)
-                  name_short = re.sub('_', '-', name_short)
+                  #name_short = re.sub('(?P<foo>\w)\w*\.', '\g<foo>.', name)
+                  name_short = re.sub('_', '-', name)
                   name_short = re.sub('\.', '_', name_short)
-                  mavio.mav.param_value_send(name_short, val, type, len(self.param_map), index)
+                  mavio.mav.param_value_send(name_short, cast(val), type, len(self.param_map), index)
                   print len(self.param_map), index, name_short, val, type
-                  sleep(0.1)
                except Exception, ex:
                   print str(ex)
          elif e.get_type() == 'PARAM_REQUEST_READ':
             print e
             index = e.param_index
-            name, type = self.param_map[index];
+            name, type, cast = self.param_map[index]
             try:
                val = self.opcd_interface.get(name)
-               name_short = re.sub('(?P<foo>\w)\w*\.', '\g<foo>.', name)
-               name_short = re.sub('_', '-', name_short)
+               #name_short = re.sub('(?P<foo>\w)\w*\.', '\g<foo>.', name)
+               name_short = re.sub('_', '-', name)
                name_short = re.sub('\.', '_', name_short)
-               sleep(0.3)
-               mavio.mav.param_value_send(name_short, val, type, len(self.param_map), index)
+               mavio.mav.param_value_send(name_short, cast(val), type, len(self.param_map), index)
                print len(self.param_map), index, name_short, val, type
             except Exception, ex:
                print str(ex)
@@ -74,10 +74,21 @@ class DeadbeefHandler(Thread):
       for e in self.dispatcher.generator('BAD_DATA'):
          print 'bad data ignored'
 
+from math import *
+
+def gps_add_meters(lat, lon, dx, dy):
+   delta_lon = dx / (111320 * cos(lat))
+   delta_lat = dy / 110540
+   new_lon = lon + delta_lon
+   new_lat = lat + delta_lat
+   return new_lat, new_lon
+
+
+
 
 from serial import Serial
 
-mavio = mavserial('/dev/ttyUSB1', 9600, source_system = 0x01)
+mavio = mavserial('/dev/ttyACM0', 115200, source_system = 0x01)
 
 source = MAVLinkSource(mavio)
 dispatcher = GenDisp(source)
@@ -107,11 +118,11 @@ Thread(target = mon_read).start()
 
 flags = 0
 while True:
-   flags += 1
-   if flags == 256:
-      flags = 0
-   
    time_ms = int(time.time() / 10)
-   mavio.mav.attitude_send(time_ms, mon.roll, mon.pitch, mon.yaw, mon.roll_speed, mon.pitch_speed, mon.yaw_speed)
+   
    mavio.mav.heartbeat_send(MAV_TYPE_QUADROTOR, MAV_AUTOPILOT_GENERIC, flags, 0, MAV_STATE_ACTIVE)
-   time.sleep(0.05)
+   lat, lon = gps_add_meters(mon.gps_start_lat, mon.gps_start_lon, mon.x, mon.y);
+   mavio.mav.global_position_int_send(time_ms, lat, lon, mon.z, 0, mon.x_speed, mon.y_speed, mon.z_speed, 0)
+   mavio.mav.attitude_send(time_ms, mon.roll, mon.pitch, mon.yaw, mon.roll_speed, mon.pitch_speed, mon.yaw_speed)
+   
+   time.sleep(0.2)
