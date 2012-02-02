b08160b7e2e5cb13b3732dbfa3428bc30dfd6f2b
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-02-02 20:56:33 +0100

    merge and fix for named_daemon

--- ARCADE/airborne/common/util/named_daemon.py
+++ ARCADE/airborne/common/util/named_daemon.py
@@ -3,21 +3,25 @@
 from sys import exit
 from signal import pause
 from daemon import DaemonContext
-from daemon import pidfile
+
+try:
+   from daemon import pidlockfile
+except:
+   from daemon import pidfile as pidlockfile
 
 
 def main_wrapper(name, main):
    main(name)
    pause()
 
-
 def daemonize(name, main):
    try:
-      pidf = pidfile.PIDLockFile('/var/run/' + name + '.pid')
+      pidf = pidlockfile.PIDLockFile('/var/run/' + name + '.pid')
       pidf.acquire(timeout = 1.0)
       pidf.release()
       with DaemonContext(pidfile = pidf):
          main_wrapper(name, main)
-   except:
+   except Exception, e:
+      print 'Could not daemonize:', str(e)
       exit(1)
 
