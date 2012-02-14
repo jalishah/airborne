3756d42d6701bf886b5c184ede9b8ac3eedc7ce4
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-02-14 21:31:19 +0100

    simplified arbiter interface

--- ARCADE/airborne/common/util/arbiter_interface.py
+++ ARCADE/airborne/common/util/arbiter_interface.py
@@ -20,16 +20,78 @@ class ArbiterInterface:
 
    def __init__(self, socket):
       self._socket = socket
-      self._lock = Lock()
 
-   def execute(self, req):
+   def _execute(self, req):
       rep = Reply()
       req_data = req.SerializeToString()
-      self._lock.acquire()
       self._socket.send(req_data)
       rep_data = self._socket.recv()
-      self._lock.release()
       rep.ParseFromString(rep_data)
       if rep.status != 0:
          raise ArbiterError('arbiter error: %s - code: %d' % (rep.message, rep.status))
+
+   def takeoff(alt = None, speed = None):
+      """takeoff"""
+      req = Request()
+      req.type = TAKEOFF
+      if alt:
+         req.pos.append(alt)
+      if speed:
+         req.speed = speed
+      self._execute(req)
+
+
+   def land(speed = None):
+      """land at current position"""
+      req = Request()
+      req.type = LAND
+      if speed:
+         req.speed = speed
+      self._execute(req)
+
+
+   def move(pos, alt = None, speed = None, rel = None):
+      """move to position"""
+      req = Request()
+      req.type = MOVE
+      if rel:
+         req.rel = rel
+      req.pos.extend(pos)
+      if alt:
+         req.pos.append(alt)
+      if speed:
+         req.speed = speed
+      self._execute(req)
+
+
+   def rotate_poi(pos, rel = None, speed = None):
+      """rotate UAV towards pos"""
+      req = Request()
+      req.type = ROT
+      req.pos.extend(pos)
+      if speed:
+         req.speed = speed
+      if rel:
+         req.rel = rel
+      self._execute(req)
+
+
+   def rotate_fixed(yaw, rel = None, speed = None):
+      """rotate UAV towards fixed angle"""
+      req = Request()
+      req.type = ROT
+      req.pos.append(yaw)
+      if speed:
+         req.speed = speed
+      if rel:
+         req.rel = rel
+      self._execute(req)
+
+
+   def stop():
+      """stop UAV at current position"""
+      req = Request()
+      req.type = STOP
+      self._execute(req)
+
 
--- ARCADE/airborne/components/interfaces/cmdctrl/cmd_interface.py
+++ ARCADE/airborne/components/interfaces/cmdctrl/cmd_interface.py
@@ -6,7 +6,6 @@ import readline
 import rlcompleter
 
 from scl import generate_map
-from arbiter_requests import Takeoff, Land, Move, RotateFixed, RotatePOI, Stop
 from arbiter_interface import ArbiterInterface
 
 
@@ -32,38 +31,39 @@ def catch_ex(func):
            print str(e)
    return wrapper
 
+
 @catch_ex
 def takeoff(alt = None, speed = None):
    """takeoff"""
-   _interface.execute(Takeoff(alt, speed)._req)
+   _interface.takeoff(alt, speed)
 
 
 @catch_ex
 def land(speed = None):
    """land at current position"""
-   _interface.execute(Land(speed)._req)
+   _interface.land(speed)
 
 
 @catch_ex
 def move(pos, alt = None, speed = None, rel = None):
    """move to position"""
-   _interface.execute(Move(pos, alt, speed, rel)._req)
+   _interface.move(pos, alt, speed, rel)
 
 
 @catch_ex
 def rotate_fixed(yaw, rel = None, speed = None):
    """rotate UAV towards fixed angle"""
-   _interface.execute(RotateFixed(yaw, rel, speed)._req)
+   _interface.rotate_fixed(yaw, rel, speed)
 
 
 @catch_ex
 def rotate_poi(pos, rel = None, speed = None):
    """rotate UAV towards pos"""
-   _interface.execute(RotatePOI(pos, rel, speed)._req)
+   _interface.rotate_poi(pos, rel, speed)
 
 
 @catch_ex
 def stop():
    """stop UAV at current position"""
-   _interface.execute(Stop()._req)
+   _interface.stop()
 
