f152993e23b2ec8a9b7c6a33b9061e775a275089
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-11-23 21:19:06 +0100

    added tcp handler

--- ARCADE/airborne/components/cal_server/cal_server.py
+++ ARCADE/airborne/components/cal_server/cal_server.py
@@ -6,24 +6,54 @@ from misc import *
 from cal_pb2 import *
 from scl import generate_map
 from core_interface import CoreInterface
+import socket
+import struct
+from time import sleep
 
 
 class CalServer:
 
    def __init__(self, cal_socket):
       self.cal_socket = cal_socket
-      self.request_thread = start_daemon_thread(self.cal_reader)
+      self.cal_reader_thread = start_daemon_thread(self.cal_reader)
+      self.cal_tcp_handler_thread = start_daemon_thread(self.cal_tcp_handler)
 
    def cal_reader(self):
       while True:
-         cal_data = CalData()
+         self.cal_data = CalData()
          try:
-            cal_data.ParseFromString(self.cal_socket.recv())
+            self.cal_data.ParseFromString(self.cal_socket.recv())
          except:
             sleep(1)
             continue
          print cal_data
 
+
+   def cal_tcp_handler(self):
+      while True:
+         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         s.bind(('', 10000))
+         s.listen(1)
+         conn, addr = s.accept()
+         while True:
+            try:
+               t = conn.recv(1)
+               if t == 'v':
+                  conn.send('ARCADE Calibration Server\r\n')
+               elif t == 'b':
+                  count = ord(conn.recv(1))
+                  for _ in range(count):
+                     raw = [self.cal_data.ax, self.cal_data.ay, self.cal_data.az,
+                            0,                0,                0, 
+                            self.cal_data.mx, self.cal_data.my, self.cal_data.mz]
+                     for r in raw:
+                        conn.send(struct.pack('h', r))
+                     conn.send('\r\n')
+                     sleep(0.05)
+            except:
+               print 'connection closed'
+         conn.close()
+         s.close()
 
 
 def main(name):
@@ -41,4 +71,5 @@ def main(name):
 
 main('cal_server')
 #daemonize('cal_server', main)
+
 
