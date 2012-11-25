05676a6a4f0e9dd7c2e7a4fd197782fbdd90590b
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-11-25 23:02:41 +0100

    added opcd interface to cal_server

--- ARCADE/airborne/components/cal_server/cal_server.py
+++ ARCADE/airborne/components/cal_server/cal_server.py
@@ -9,12 +9,14 @@ from core_interface import CoreInterface
 import socket
 import struct
 from time import sleep
+from opcd_interface import OPCD_Interface
 
 
 class CalServer:
 
-   def __init__(self, cal_socket):
+   def __init__(self, cal_socket, opcd):
       self.cal_socket = cal_socket
+      self.opcd = opcd
       self.cal_reader_thread = start_daemon_thread(self.cal_reader)
       self.cal_tcp_handler_thread = start_daemon_thread(self.cal_tcp_handler)
 
@@ -36,6 +38,8 @@ class CalServer:
          s.listen(1)
          conn, addr = s.accept()
          print 'new connection'
+         ACC_SCALE = 1000.0
+         MAG_SCALE = 10.0
          while True:
             try:
                t = conn.recv(1)
@@ -44,13 +48,25 @@ class CalServer:
                elif t == 'b':
                   count = ord(conn.recv(1))
                   for _ in range(count):
-                     raw = [self.cal_data.ax * 1000.0, self.cal_data.ay * 1000.0, self.cal_data.az * 1000.0,
+                     raw = [self.cal_data.ax * ACC_SCALE, self.cal_data.ay * ACC_SCALE, self.cal_data.az * ACC_SCALE,
                             0,                0,                0, 
-                            self.cal_data.mx * 10.0, self.cal_data.my * 10.0, self.cal_data.mz * 10.0]
+                            self.cal_data.mx * MAG_SCALE, self.cal_data.my * MAG_SCALE, self.cal_data.mz * MAG_SCALE]
                      for r in raw:
                         conn.send(struct.pack('h', int(r)))
                      conn.send('\r\n')
                      sleep(0.01)
+               elif t == 'c':
+                  dim = {0: 'x', 1: 'y', 2: 'z'}
+                  acc_biases = map(lambda x: x / ACC_SCALE, struct.unpack('<hhh', conn.recv(3 * 2)))
+                  mag_biases = map(lambda x: x / MAG_SCALE, struct.unpack('<hhh', conn.recv(3 * 2)))
+                  acc_scales = struct.unpack('<fff', conn.recv(3 * 4)) # TODO: scaling?
+                  mag_scales = struct.unpack('<fff', conn.recv(3 * 4)) # ?
+                  print acc_biases, acc_scales, mag_biases, mag_scales
+                  for i in range(3):
+                     self.opcd.set('core.cal.acc_bias_%d' % dim[i], acc_biases[i])
+                     self.opcd.set('core.cal.mag_bias_%d' % dim[i], mag_biases[i])
+                     self.opcd.set('core.cal.acc_scale_%d' % dim[i], acc_scales[i])
+                     self.opcd.set('core.cal.mag_scale_%d' % dim[i], mag_scales[i])
             except:
                print 'connection closed'
          conn.close()
@@ -59,11 +75,12 @@ class CalServer:
 
 def main(name):
    map = generate_map(name)
-   core = CoreInterface(map['ctrl'], None)
+   core = CoreInterface(map['core_ctrl'], None)
    # configure core to operate in calibration mode:
    core.mode_cal()
    cal_socket = map['cal']
-   CalServer(cal_socket)
+   opcd = OPCD_Interface(map['opcd_opcd'])
+   CalServer(cal_socket, opcd)
    await_signal()
    # configure core to operate in normal mode:
    core.mode_normal()
