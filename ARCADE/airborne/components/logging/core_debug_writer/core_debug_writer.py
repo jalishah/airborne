5b52c6cdf7a8688a51eefe0128ba0a774c7a343e
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-12-10 18:23:45 +0100

    moved from lzma back to zlib

--- ARCADE/airborne/components/logging/core_debug_writer/core_debug_writer.py
+++ ARCADE/airborne/components/logging/core_debug_writer/core_debug_writer.py
@@ -3,22 +3,19 @@
 from os import sep
 from scl import generate_map
 from misc import daemonize, user_data_dir
-from lzma import LZMACompressor
 from datetime import datetime
+import gzip
 
 
 def main(name):
    socket = generate_map(name)['debug']
-   comp = LZMACompressor()
    try:
       now = datetime.today().isoformat()
-      f = open(user_data_dir() + sep + 'core_debug_%s.xz' % now, "wb")
+      f = gzip.open(user_data_dir() + sep + 'core_debug_%s.gz' % now, "wb")
       while True:
          line = socket.recv()
          f.write(comp.compress(line + '\n'))
-         f.flush()
    finally:
-      f.write(comp.flush())
       f.close()
 
 
