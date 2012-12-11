f612d454f4956549ac86bc3d2c89aa7df3793449
TobiasSimon <tobias.simon@tu-ilmenau.de>
2012-12-11 19:17:50 +0100

    minor changes

--- ARCADE/airborne/components/logging/core_debug_writer/core_debug_writer.py
+++ ARCADE/airborne/components/logging/core_debug_writer/core_debug_writer.py
@@ -4,20 +4,20 @@ from os import sep
 from scl import generate_map
 from misc import daemonize, user_data_dir
 from datetime import datetime
-import gzip
 
 
 def main(name):
    socket = generate_map(name)['debug']
    try:
-      now = datetime.today().isoformat()
-      f = gzip.open(user_data_dir() + sep + 'core_debug_%s.gz' % now, "wb")
+      now = datetime.today().isoformat().replace(':', '')
+      f = open(user_data_dir() + sep + 'core_debug_%s.log' % now, "wb")
       while True:
          line = socket.recv()
-         f.write(comp.compress(line + '\n'))
+         f.write(line + '\n')
    finally:
       f.close()
 
 
-daemonize('core_debug_writer', main)
+main('core_debug_writer')
+#daemonize('core_debug_writer', main)
 
